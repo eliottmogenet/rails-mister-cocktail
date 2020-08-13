5724b2c66bb4c3b56184676e6e5e5d7d4c1834c1
Eliott Mogenet <eliott.mogenet@gmail.com>
2020-08-13 15:34:13 +0200

    cocktail back

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+Ingredient.create(name: "strawberry")
+Ingredient.create(name: "ananas")
+
+Cocktail.create(name: "Super cocktail 1")
+Cocktail.create(name: "Super cocktail 2")
